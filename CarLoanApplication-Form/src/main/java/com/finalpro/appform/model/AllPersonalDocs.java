75922101d279ef9cab2c577cbc3c94568663c623
shweta <shwetadhopre@gmail.com>
2024-06-06 12:47:16 +0530

    AllPersonalDocs entity class added !..

--- CarLoanApplication-Form/src/main/java/com/finalpro/appform/model/AllPersonalDocs.java
+++ CarLoanApplication-Form/src/main/java/com/finalpro/appform/model/AllPersonalDocs.java
@@ -1,5 +1,49 @@
 package com.finalpro.appform.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Data;
+
+
+@Data
+@Entity
 public class AllPersonalDocs {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int documentId;
+	@Lob
+	@Column(length = 999999999)
+	private byte[] addressProof;
+	@Lob
+	@Column(length = 999999999)
+	private byte[] panCard;
+	@Lob
+	@Column(length = 999999999)
+	private byte[] incomeTax;
+
+	@Lob
+	@Column(length = 999999999)
+	private byte[] adharCard;
+
+	@Lob
+	@Column(length = 999999999)
+	private byte[] photo;
+
+	@Lob
+	@Column(length = 999999999)
+	private byte[] signature;
+
+	@Lob
+	@Column(length = 999999999)
+	private byte[] bankCheque;
+
+	@Lob
+	@Column
+	private byte[] salarySlips;
 
 }
